b42756ae8ba8978e6347b7d3e865e3c7b9720519
JeonIlGyu <illgyu9189@gmail.com>
2022-05-14 02:20:25 +0900

    22-05-14

---
+++ src/baekJoon/BaekJoon2908.java
@@ -0,0 +1,14 @@
+package baekJoon;
+
+public class BaekJoon2908 {
+    public static void main(String[] args) {
+        int i; // 변수선언
+        i = 1; // = 은 대입 1 초기화
+        int j = 1; // 변수 선언 및 초기화
+
+        int sum = 5;
+
+        float avg = (float) (sum / 24.0); // double
+
+    } // main
+}

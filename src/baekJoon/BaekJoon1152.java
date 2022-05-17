2c3b7e021a20289e472ebdc81c1bb06c3372bfe5
JeonIlGyu <illgyu9189@gmail.com>
2022-05-17 16:31:42 +0900

    22-05-17 16:31

--- src/baekJoon/BaekJoon1152.java
+++ src/baekJoon/BaekJoon1152.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BaekJoon1152 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {  
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         String s = bufferedReader.readLine();
--- src/baekJoon/BaekJoon2908.java
+++ src/baekJoon/BaekJoon2908.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BaekJoon2908 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-                   
+
         int num = scanner.nextInt();
         int num2 = scanner.nextInt();
 

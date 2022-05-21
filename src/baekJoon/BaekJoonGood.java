e14be79af20ec3735041786a4a6825740b2f9705
JeonIlGyu <illgyu9189@gmail.com>
2022-05-21 20:43:24 +0900

    22-05-21 20:43

---
+++ src/baekJoon/BaekJoonGood.java
@@ -0,0 +1,13 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoonGood {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+
+        System.out.println(id+"??!");
+    }
+}

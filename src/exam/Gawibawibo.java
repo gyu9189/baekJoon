22e06a3d0f57ece0c0fec50aa247bba6ac2d70cc
JeonIlGyu <illgyu9189@gmail.com>
2022-06-10 17:06:26 +0900

    22-06-10 17:06

--- src/exam/Gawibawibo.java
+++ src/exam/Gawibawibo.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
-public class Gawibawibo {
+public class Gawibawibo { // pp
 
 	// ����� ��ü Ÿ�� �ʵ� ����.
 	static private Checker checker;
